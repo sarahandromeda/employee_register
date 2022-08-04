74ad6e70d3f83efd99ccb98e1e8526b9aba8b89a
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-08-04 17:16:14 -0600

    Added code to allow database connection.

--- employee_register/employee_register/settings.py
+++ employee_register/employee_register/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import dotenv
 import os
+import dotenv
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,3 +147,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
