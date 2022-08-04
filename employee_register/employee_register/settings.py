0e678f00c975ae017f95a5e355897e4e874488a5
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-08-04 14:34:54 -0600

    Integrated postgres database into project. Ready to deploy.

--- employee_register/employee_register/settings.py
+++ employee_register/employee_register/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'employee_register.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'register_app',
+        'USER': 'SarahG',
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
