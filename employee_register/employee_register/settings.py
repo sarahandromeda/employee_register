0e09275a507028c4b6d43819a8e67617b7e77474
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-08-04 15:58:07 -0600

    Updated secret key code.

--- employee_register/employee_register/settings.py
+++ employee_register/employee_register/settings.py
@@ -25,7 +25,7 @@ if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
